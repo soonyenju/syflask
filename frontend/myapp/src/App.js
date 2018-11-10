5f3e8ba96847067df5aa0737ce8333d8f39403b1
Soonyen Ju <soonyenju@foxmail.com>
2018-11-10 11:05:15 +0800

    cli

---
+++ frontend/myapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
